fc3d1e9f027fb6f4642aa8d0fe6bd29f44c07dca
Henrique Sa <henriquepss@gmail.com>
2015-04-24 17:15:07 -0500

    Added gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
- var gulp = require('gulp');
+var gulp = require('gulp');
 var browserify = require('browserify');
 var transform = require('vinyl-transform');
 var uglify = require('gulp-uglify');
@@ -6,17 +6,24 @@ var rename = require('gulp-rename');
 var insert = require('gulp-insert');
 var package = require('./package.json');
 
-var browserified = transform(function(filename) {
-  return browserify(filename)
-    .bundle();
-});
-
 var config = {};
 config.browserBuild = true;
 config.versionString = "// Appbase Javascript Library v" + package.version + "\n";
+config.pathScripts = './src';
+config.src = "./index.js";
+
+gulp.task('default', ['watch']);
+
+gulp.task('watch', ['build'], function() {
+  gulp.watch(config.pathScripts + '/**/*.js', ['build']);
+});
 
 gulp.task('build', function () {
-  config.src = "./index.js";
+  var browserified = transform(function(filename) {
+    return browserify(filename)
+      .bundle();
+  });
+
   config.dest = "./dist";
   config.destFile = "appbase.min.js";
   return gulp.src([config.src])
