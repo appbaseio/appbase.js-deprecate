f7e63c0477d612b15cc1f537f11beae8f26cbaab
Pedro Assis <pedro@pedroassis.com.br>
2015-04-24 21:54:34 -0300

    Added tests to Collection

--- test/collectionTest.js
+++ test/collectionTest.js
@@ -13,6 +13,10 @@ var Collection = require("../src/Collection");
 describe('Collection Behavior', function() {
     var collection;
 
+    beforeEach(function() {
+        collection = new Collection("someName", request, URL);
+    });
+
     afterEach(function() {
         Object.keys(request).forEach(function(item) {
             request[item].restore ? request[item].restore() : null;
@@ -30,7 +34,7 @@ describe('Collection Behavior', function() {
 
             sinon.stub(request, 'post').returns(deferred);
 
-            new Collection("someName", request, URL).search({}).then(function(response) {
+            collection.search({}).then(function(response) {
                 expect(data).to.equal(response);
             })
             .then(done);
@@ -44,10 +48,74 @@ describe('Collection Behavior', function() {
 
             sinon.stub(request, 'post');
 
-            new Collection("someName", request, URL).search(query);
+            collection.search(query);
 
             expect(request.post.args[0][0].indexOf(URL.SEARCH) !== -1).to.be.ok;
             expect(request.post.args[0][1] === query).to.be.ok;
+        });
+
+        it("Should get a Document from the collection", function() {
+
+            var data = {
+                _id : "name"
+            };
+
+            var deferred = new Promise(function(resolver) {
+                resolver(data);
+            });
+
+            sinon.stub(request, 'get').returns(deferred);
+
+            collection.get(data._id).then(function(response) {
+                expect(data._id).to.equal(response._id);
+            });
+
+            expect(request.get.calledWith("/someName/" + data._id + URL.PROPERTIES)).to.be.ok;
+
+        });
+
+        it("Should get all Documents from the collection", function() {
+
+            var data = [{
+                _id : "name"
+            }];
+
+            var filter = {
+                test : 'dsfgh'
+            };
+
+            var deferred = new Promise(function(resolver) {
+                resolver(data);
+            });
+
+            sinon.stub(request, 'get').returns(deferred);
+
+            collection.getAll(filter).then(function(response) {
+                expect(data).to.equal(response);
+            });
+
+            expect(request.get.calledWith("/someName" + URL.DOCUMENTS, filter)).to.be.ok;
+
+        });
+
+        it("Should get all Documents from the collection with a empty filter", function() {
+
+            var data = [{
+                _id : "name"
+            }];
+
+            var deferred = new Promise(function(resolver) {
+                resolver(data);
+            });
+
+            sinon.stub(request, 'get').returns(deferred);
+
+            collection.getAll().then(function(response) {
+                expect(data).to.equal(response);
+            });
+
+            expect(request.get.calledWithMatch("/someName" + URL.DOCUMENTS, {})).to.be.ok;
+
         });
     });
 });
